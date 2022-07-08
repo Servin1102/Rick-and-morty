b7f207abb3c469b12077a0e6c1bacab19a951194
Juan Martinez <juanmaa1102@gmail.com>
2022-07-08 07:43:59 -0500

    cambios

--- src/Components/CharacterItem.jsx
+++ src/Components/CharacterItem.jsx
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import axios from "axios"
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const CharacterItem = ({ locationurl }) => {
+  const [characterdata, setcharacterdata] = useState({});
+  let component = null;
 
-    const  [ characterdata, setcharacterdata ] = useState({})
+  useEffect(() => {
+    axios.get(locationurl).then((res) => setcharacterdata(res.data));
+  }, []);
 
-    useEffect(() =>{
-        axios.get(locationurl)
-        .then(res => setcharacterdata(res.data))
-    }, [])
+  console.log(characterdata);
 
-    console.log(characterdata);
+  if (characterdata.status === "Alive") {
+    component = "circle-green";
+  }
+  if (characterdata.status === "unknown") {
+    component = "circle-grey";
+  }
+  if (characterdata.status === "Dead") {
+    component = "circle-red";
+  }
 
-    return (
-        <div>
-            <div className="container mt-5">
-          <div className="row">
-          <div className="col-md-6">
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row">
+          <div className=" col-md-6">
             <div className="cards">
               <img src={characterdata.image} alt="" />
               <div className="info">
@@ -25,20 +34,20 @@ const CharacterItem = ({ locationurl }) => {
                   <b>{characterdata?.name}</b>
                 </p>
                 <p>
-                  <span className="circle-green"></span>
+                  <span className={component}></span>
                   {characterdata.status} - {characterdata.species}
                 </p>
                 <p className="description">origin:</p>
                 <p>{characterdata.origin?.name}</p>
                 <p className="description">Episodes where appear:</p>
                 <p>{characterdata.episode?.length}</p>
-                </div>
               </div>
             </div>
           </div>
-          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CharacterItem;
